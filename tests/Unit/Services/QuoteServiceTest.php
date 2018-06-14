499f1e6414915e850edf340225f9c575cfa7a282
Andrejs Semovs <andrejs.semovs@gmail.com>
2018-06-14 03:12:56 +0200

    Add unit test for quote calculation

--- tests/Unit/Services/QuoteServiceTest.php
+++ tests/Unit/Services/QuoteServiceTest.php
@@ -2,12 +2,71 @@
 
 namespace Tests\Unit\Services;
 
+use App\Models\Product;
+use App\Services\QuoteService;
+use PHPUnit\Framework\MockObject\MockObject;
 use Tests\TestCase;
 
 class QuoteServiceTest extends TestCase
 {
-    public function testCalculate()
+    /**
+     * @param array $productRequest
+     * @param array $prices
+     * @param int $expected
+     * @dataProvider calculateDataProvider
+     */
+    public function testCalculate($productRequest, $prices, $expected)
     {
-        $this->assertTrue(true);
+        /** @var QuoteService|MockObject $service */
+        $service = $this->getMockBuilder(QuoteService::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getProductPrices'])
+            ->getMock();
+
+        $service
+            ->expects($this->once())
+            ->method('getProductPrices')
+            ->willReturn($prices);
+
+        $this->assertEquals($expected, $service->calculate($productRequest));
+    }
+
+    /**
+     * @return array
+     */
+    public function calculateDataProvider()
+    {
+        return [
+            [
+                [
+                    [
+                        'product_id' => 1,
+                        'quantity' => 5,
+                    ],
+                    [
+                        'product_id' => 3,
+                        'quantity' => 3,
+                    ],
+                    [
+                        'product_id' => 7,
+                        'quantity' => 9,
+                    ],
+                ],
+                [
+                    $this->makeProductPrice(1, 2300),
+                    $this->makeProductPrice(3, 1250),
+                    $this->makeProductPrice(7, 2599),
+                ],
+                38641,
+            ]
+        ];
+    }
+
+    protected function makeProductPrice($id, $price)
+    {
+        $product = new Product(['price' => $price]);
+        $product->id = $id;
+
+        return $product;
     }
 }
